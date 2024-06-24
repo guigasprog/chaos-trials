624e5d0af1d6c75ffa0660985d2ef25e97426969
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-06-24 11:11:18 -0300

    feat: inclementar domain e entidade Account

---
+++ back/src/main/java/com/chaostrials/game/domain/entity/account/Account.java
@@ -0,0 +1,25 @@
+package com.chaostrials.game.domain.entity.account;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(schema = "private", name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+}
