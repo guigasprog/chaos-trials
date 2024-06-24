95b53313734eafd9b0330580947e4d7966da2054
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-06-24 13:18:40 -0300

    fix: arrumar controller Account

--- back/src/main/java/com/chaostrials/game/entrypoint/rest/AccountController.java
+++ back/src/main/java/com/chaostrials/game/entrypoint/rest/AccountController.java
@@ -1,5 +1,30 @@
 package com.chaostrials.game.entrypoint.rest;
 
+import com.chaostrials.game.domain.entity.account.Account;
+import com.chaostrials.game.domain.forms.LoginForm;
+import com.chaostrials.game.usecase.account.LoginAccount;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Resource
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private LoginAccount loginAccount;
+
+    AccountController(LoginAccount loginAccount) {
+        this.loginAccount = loginAccount;
+    }
+
+    @GetMapping
+    public ResponseEntity<Account> login(LoginForm loginForm) {
+        return ResponseEntity.ok().body(this.loginAccount.loginAccount(loginForm));
+    }
 
 }
