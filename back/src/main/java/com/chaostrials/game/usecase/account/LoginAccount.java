218ebade89cac5db95a0b343ed78dd513a424a01
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-06-24 13:18:25 -0300

    fix: arrumar usecase login Account

--- back/src/main/java/com/chaostrials/game/usecase/account/LoginAccount.java
+++ back/src/main/java/com/chaostrials/game/usecase/account/LoginAccount.java
@@ -3,7 +3,7 @@ package com.chaostrials.game.usecase.account;
 import com.chaostrials.game.domain.entity.account.Account;
 import com.chaostrials.game.domain.forms.LoginForm;
 import com.chaostrials.game.domain.repository.AccountRepository;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,13 +11,14 @@ import java.util.Optional;
 @Service
 public class LoginAccount {
 
+    @Autowired
     private final AccountRepository accountRepository;
 
     LoginAccount(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
-    private Account loginAccount(LoginForm loginForm) {
+    public Account loginAccount(LoginForm loginForm) {
         if(loginForm.getPassword().equals(loginForm.getConfirmPassword())) {
             Optional<Account> account = this.accountRepository.findByEmail(loginForm.getEmail());
             if(account.isPresent()) {
