59209ada6ab7b95d3203585df547180806f5cf97
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-06-25 13:12:39 -0300

    fix: arrumar validate do Forms de Login

--- back/src/main/java/com/chaostrials/game/domain/forms/LoginForm.java
+++ back/src/main/java/com/chaostrials/game/domain/forms/LoginForm.java
@@ -1,16 +1,24 @@
 package com.chaostrials.game.domain.forms;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginForm {
-    
+
+    @Size(max = 125, message = "Erro LoginForm => Campo Email passou do tamanho maximo {max}")
+    @NotEmpty(message = "Erro LoginForm => Campo Email não pode ser Nulo ou em Branco")
     private String email;
 
+    @Size(max = 120, message = "Erro LoginForm => Campo Password passou do tamanho maximo {max}")
+    @NotEmpty(message = "Erro LoginForm => Campo Password não pode ser Nulo ou em Branco")
     private String password;
 
+    @Size(max = 120, message = "Erro LoginForm => Campo ConfirmPassword passou do tamanho maximo {max}")
+    @NotEmpty(message = "Erro LoginForm => Campo ConfirmPassword não pode ser Nulo ou em Branco")
     private String confirmPassword;
 
 }
