13950ac21a70f44716385a09b0fcbabe29608907
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-06-28 09:53:26 -0300

    fix: arrumar entidade RequestGuild

--- back/src/main/java/com/chaostrials/game/domain/entity/guild/RequestGuild.java
+++ back/src/main/java/com/chaostrials/game/domain/entity/guild/RequestGuild.java
@@ -1,25 +1,33 @@
 package com.chaostrials.game.domain.entity.guild;
 
+import com.chaostrials.game.domain.entity.character.Character;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
-@Table(schema = "private", name = "request")
+@Table(schema = "private", name = "request_guild")
 public class RequestGuild {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
 
+    private LocalDateTime dateTimeOfRequest;
+
     private String description;
 
-    private UUID uuidGuild;
+    @ManyToOne
+    @JoinColumn(name = "uuid_guild")
+    private Guild uuidGuild;
 
-    private UUID uuidCharacter;
+    @ManyToOne
+    @JoinColumn(name = "uuid_character")
+    private Character uuidCharacter;
 
 }
