a3bbe1ab62b23dc37eb61751583d12e1b94cb8cf
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-08-05 12:25:57 -0300

    feat: inclementar primeira parte do component de login, sign-in component

---
+++ front/src/app/view/login/signin/sign-in.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'VIEW-SIGN-IN',
+  template: `
+
+  `,
+  styles: `
+
+  `
+})
+export class ViewLoginSignIn {
+
+}
