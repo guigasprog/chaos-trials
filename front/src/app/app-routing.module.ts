1df0e8886cd896ce3282ebf96565bca8c0baac30
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-08-05 12:26:30 -0300

    feat: inclementar router

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewLogin } from './view/login/login.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
-
+  {
+    path: 'cards',
+    component: CardComponent
+  },
+  {
+    path: '',
+    component: ViewLogin
+  }
 ];
 
 @NgModule({
